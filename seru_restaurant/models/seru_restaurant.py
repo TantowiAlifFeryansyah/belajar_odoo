13fbc7e392b93add92283f5289e0b8704eff5c7b
Tantowi <tantowialif15@gmail.com>
2023-08-25 19:52:34 +0700

    day 5 sesi 1

--- seru_restaurant/models/seru_restaurant.py
+++ seru_restaurant/models/seru_restaurant.py
@@ -9,3 +9,24 @@ class SeruMenu(models.Model):
 
     name = fields.Char()
     harga = fields.Float()
+
+    state = fields.Selection(string="Status", selection=[
+        ('draft', 'Draft'),
+        ('confirm', 'Confirm'),
+        ('reject', 'Reject')
+    ], default='draft', )
+
+    image = fields.Binary(string="Image",  )
+    attachment_ids = fields.Many2many(comodel_name="ir.attachment", string="Attachments", )
+
+    def action_confirm(self):
+        self.state = 'confirm'
+        return
+
+class SeruRestaurant(models.Model):
+    _name = 'seru.restaurant'
+    _rec_name = 'name'
+    _description = 'Seru Restaurant'
+
+    name = fields.Char()
+    alamat = fields.Text()
