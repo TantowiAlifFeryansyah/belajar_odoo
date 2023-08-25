2d55881bbf7dca97d12d17f6bb1393d705bce244
Fadhillah Akbar <mfadhillahakbar14@gmail.com>
2023-08-25 22:07:40 +0700

    add netto dan diskon

--- seru_restaurant/models/seru_restaurant.py
+++ seru_restaurant/models/seru_restaurant.py
@@ -9,6 +9,7 @@ class SeruMenu(models.Model):
 
     name = fields.Char()
     harga = fields.Float()
+    no_menu = fields.Char()
 
     state = fields.Selection(string="Status", selection=[
         ('draft', 'Draft'),
@@ -20,6 +21,12 @@ class SeruMenu(models.Model):
     attachment_ids = fields.Many2many(comodel_name="ir.attachment", string="Attachments", )
     # new_field_ids = fields.One2many(comodel_name="", inverse_name="", string="", required=False, )
     # new_field_id = fields.Many2one(comodel_name="", string="", required=False, )
+
+    @api.model
+    def create(self, values):
+        values['no_menu'] = self.env.ref('seru_restaurant.seq_restaurant_menu').next_by_id()
+        res = super(SeruMenu, self).create(values)
+        return res
 
     def action_confirm(self):
         self.state = 'confirm'
@@ -51,4 +58,22 @@ class RestaurantMenu(models.Model):
 
     harga = fields.Float()
     harga_menu = fields.Float(related="menu_id.harga")
+    diskon = fields.Float()
+    netto = fields.Float(compute='compute_netto')
+    voucher = fields.Float()
+    setelah_voucher = fields.Float(compute='compute_voucher',store=True)
+
+    @api.onchange('harga', 'diskon')
+    def compute_netto(self):
+        for me in self:
+            #import ipdb;ipdb.set_trace()
+            me.netto = me.harga-(me.diskon*me.harga/100)
+        return
+
+    @api.onchange('harga', 'voucher')
+    def compute_voucher(self):
+        for me in self:
+            me.setelah_voucher = me.harga-me.voucher
+
+
 
