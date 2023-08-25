cd8a5a96f52bf720d4efde03b0f02be690c1b52c
Tantowi <tantowialif15@gmail.com>
2023-08-25 21:03:23 +0700

    wip sequences

--- seru_restaurant/models/seru_restaurant.py
+++ seru_restaurant/models/seru_restaurant.py
@@ -30,5 +30,20 @@ class SeruRestaurant(models.Model):
     _rec_name = 'name'
     _description = 'Seru Restaurant'
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     alamat = fields.Text()
+    no_handphone = fields.Char(readonly=True)
+    no_induk = fields.Char()
+
+    menu_ids = fields.One2many(comodel_name="restaurant.menu", inverse_name="restaurant_id", string="Menu", required=False, )
+
+class RestaurantMenu(models.Model):
+    _name = 'restaurant.menu'
+    _description = 'Restaurant Menu'
+
+    restaurant_id = fields.Many2one(comodel_name="seru.restaurant", string="Restaurant", required=False, )
+    menu_id = fields.Many2one(comodel_name="seru.menu", string="Menu", required=False, )
+
+    harga = fields.Float()
+    harga_menu = fields.Float(related="menu_id.harga")
+
